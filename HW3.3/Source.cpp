58cc16dfc5f5b2c591db158c395f01e119096e4e
NUTTANAN RUANGPHANICH <63010320@KMITL.AC.TH>
2020-08-30 19:58:27 +0700

    Add project files.

---
+++ HW3.3/Source.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int num;
+	scanf_s("%d", &num);
+	for (int i = 1; i <= num; i++) {
+		for (int j = 1; j <= num; j++) {
+			if (i == 1 || i == num || j == 1 || j == num || i == j) {
+				printf("*");
+			}
+			else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
